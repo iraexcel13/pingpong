34fb5cb913f05bb6d2a22c59b756800eebf3ea2b
Iroveena Pagsibigan <iraexcel13@gmail.com>
2017-06-24 20:22:39 -0700

    Add business logic and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 var userNumber = function(myNumber){
-  if ((myNumber % 3 === 0)){
-    return true;
-  }
-  else {
-    return false;
-  }
+// var num = 1
+  for (var i=userNumber; i<=userNumber; i++)
+    if (userNumber % 3 === 0) {
+      return ("Ping");
+    }
+    else if (userNumber % 5 === 0) {
+      return ("Pong");
+    }
+    else if (userNumber % 15 === 0) {
+      return ("PingPong");
+    }
+    else {
+      return ("Invalid");
+    }
+//  }
 };
 
 $(document).ready(function(){
